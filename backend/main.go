2189defdcb58ede1286d9e5725a3d2e056c473bd
YAWAL <yavorskyyval@gmail.com>
2017-10-13 12:08:37 +0300

    build pb.go file

--- backend/main.go
+++ backend/main.go
@@ -1,5 +1,19 @@
 package backend
 
+import (
+	"flag"
+	"net"
+	"fmt"
+)
+
 func main(){
 
+	port := flag.Int("p", 8080, "port to listen to")
+	flag.Parse()
+
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+
+	if err != nil{
+
+	}
 }

b1a314a7a8b1d15796aa97becab375e3a58b20b9
YAWAL <gajkaGAJKA140889>
2017-10-19 16:45:43 +0300

    download service implementation

---
+++ backend/downloadPic/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"flag"
+	"net"
+	"fmt"
+	"log"
+	api "github.com/YAWAL/gRPCtrying/api"
+
+	"google.golang.org/grpc"
+	"golang.org/x/net/context"
+	"io/ioutil"
+)
+
+func main(){
+
+	port := flag.Int("p", 50111, "port to listen to")
+	flag.Parse()
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+	if err != nil{
+		log.Fatalf("Error has occured %d: %v", port, err)
+	}
+	srv := grpc.NewServer()
+	api.RegisterDownloadPicServer(srv, server{})
+	err = srv.Serve(lis)
+	if err != nil{
+		log.Fatalf("Error has occured: %v", err)
+	}
+}
+
+type server struct {}
+
+func (server) Download(ctx context.Context, id *api.Id) (*api.Picture, error) {
+	defer ctx.Done()
+	pic, err := ioutil.ReadFile(id.Id)
+	if err != nil{
+		log.Printf("Error has occured: %v", err)
+		return &api.Picture{}, err
+	}
+	picture := &api.Picture{Pic:pic}
+	return picture, nil
+}

1984b69679190567d88f89214ceffac61fc20ba4
YAWAL <gajkaGAJKA140889>
2017-10-19 17:01:54 +0300

    client implementation

---
+++ client/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"context"
+	"time"
+	"google.golang.org/grpc"
+	api "github.com/YAWAL/gRPCtrying/api"
+
+	"io/ioutil"
+)
+
+func main() {
+	backend := flag.String("backend", "172.18.0.3.50111", "port for connection to download server")
+	flag.Parse()
+
+	cntx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	conn, err := grpc.DialContext(cntx, *backend, grpc.WithInsecure(), grpc.WithBlock())
+	defer conn.Close()
+	if err != nil {
+		log.Fatalf("Error has occured %v:", err)
+	}
+	client := api.NewSearchPicClient(conn)
+	name := &api.Name{Name: "gopher"}
+	picture, err := client.Search(context.Background(), name)
+	if err != nil {
+		log.Fatalf("Error has occured %v:", err)
+	}
+	if err = ioutil.WriteFile("gopher1.png", picture.Pic, 0666); err != nil {
+		log.Fatalf("Error has occured %v:", err)
+		return
+	}
+	log.Println("File has been created")
+}
